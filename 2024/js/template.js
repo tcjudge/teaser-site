7ae8629ec19be5b7d3ad02787a6b2ee4d8e6289d
Kawa_ <yuyux0910@gmail.com>
2024-11-21 04:32:12 +0900

    [update] Bootstrapのバージョンをアップデート
    Bootstrapのバージョンを5.3にアップデートした
    バージョンアップに際して一部のUIのデザインを変更した

--- 2024/js/template.js
+++ 2024/js/template.js
@@ -1,28 +1,30 @@
 $(document).ready(function () {
     var navbar_html = (function () { /*
-            <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2" aria-controls="exCollapsingNavbar2" aria-expanded="false" aria-label="Toggle navigation">
-            &#9776;
-            </button>
-            <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary fixed-top">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
                 <a class="navbar-brand" href="index.html"><img src="images/teracoder.png"/></a>
-                <ul class="nav navbar-nav">
-                    <li class="nav-item" id="nav-item1">
-                        <a class="nav-link" href="index.html" id="nav-item1-text">ホーム</a>
-                    </li>
-                    <li class="nav-item" id="nav-item2">
-                        <a class="nav-link" href="rule.html" id="nav-item2-text">ルール</a>
-                    </li>
-                    <li class="nav-item" id="nav-item3">
-                        <a class="nav-link" href="prize.html" id="nav-item3-text">賞品</a>
-                    </li>
-                    <li class="nav-item" id="nav-item4">
-                        <a class="nav-link" href="practice.html" id="nav-item4-text">練習</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="http://tcjudge.kyoto-su.ac.jp">会場</a>
-                    </li>
-                </ul>
-            </div>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="nav navbar-nav">
+                        <li class="nav-item" id="nav-item1">
+                            <a class="nav-link" href="index.html" id="nav-item1-text">ホーム</a>
+                        </li>
+                        <li class="nav-item" id="nav-item2">
+                            <a class="nav-link" href="rule.html" id="nav-item2-text">ルール</a>
+                        </li>
+                        <li class="nav-item" id="nav-item3">
+                            <a class="nav-link" href="prize.html" id="nav-item3-text">賞品</a>
+                        </li>
+                        <li class="nav-item" id="nav-item4">
+                            <a class="nav-link" href="practice.html" id="nav-item4-text">練習</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://tcjudge.kyoto-su.ac.jp">会場</a>
+                        </li>
+                    </ul>
+                </div>
+        </nav>
     */}).toString().split("*")[1];
     $("#navbar").append(navbar_html);
 
@@ -30,19 +32,15 @@ $(document).ready(function () {
     console.log(filename);
     if (filename == "index.html") {
         $('#nav-item1').addClass("active");
-        $('#nav-item1-text').append(' <span class="sr-only">(current)</span>');
     }
     else if (filename == "rule.html") {
         $('#nav-item2').addClass("active");
-        $('#nav-item2-text').append(' <span class="sr-only">(current)</span>');
     }
     else if (filename == "prize.html") {
         $('#nav-item3').addClass("active");
-        $('#nav-item3-text').append(' <span class="sr-only">(current)</span>');
     }
     else if (filename == "practice.html") {
         $('#nav-item4').addClass("active");
-        $('#nav-item4-text').append(' <span class="sr-only">(current)</span>');
     }
 
     function getDaysLeft(y, m, d) {
